398e91c461861845bdb9103117aeb12561bbcd57
creatorbe <creatorb45@gmail.com>
2024-12-05 14:54:05 +0700

    :art: feat : profile

--- lib/const.dart
+++ lib/const.dart
@@ -4,8 +4,8 @@ class Const {
   static const String URL_WEB = 'https://med-map.org';
   static const String URL_WEB_DETAIL_CASE = URL_WEB + '/cases-analysis/detail';
   static const String URL_WEB_DETAIL_PRODUCT = URL_WEB + '/product-detail';
-  // static const String BASE_URL = 'http://192.168.50.100:3333';
-  static const String BASE_URL = 'https://api.med-map.org';
+  static const String BASE_URL = 'http://192.168.50.100:3333';
+  // static const String BASE_URL = 'https://api.med-map.org';
   // static const String BASE_URL = 'https://api-medmap.mandatech.co.id';
   // static const String BASE_URL = 'https://be-mdmap.mandatech.co.id/';
   static const String URL_API = BASE_URL + '/v1';
@@ -22,6 +22,7 @@ class Const {
 
   static const String URL_PRIVACY = URL_WEB + '/privacy-policy';
 
+  static const String ROLE = 'role';
   static const String IS_LOGED_IN = 'is_logged_in';
   static const String TOKEN = 'token';
   static const String EXPIRES_AT = 'expires_at';
--- lib/route/app_routes.dart
+++ lib/route/app_routes.dart
@@ -7,6 +7,7 @@ class AppRoutes {
   static const String partnership = '/request-partnership';
   static const String partnership_list = '/partnership-list';
   static const String service_request = '/service-request';
+  static const String profile = '/profile';
   // static const String home = '/';
   // static const String submenu = 'submenu';
   // static const String dashboard= 'dashboard';
