26c5cff6235d099950e81b66373c3b6b24f2f3f4
creatorbe <creatorb45@gmail.com>
2024-12-05 14:57:22 +0700

    :hammer: update api url

--- lib/const.dart
+++ lib/const.dart
@@ -4,8 +4,8 @@ class Const {
   static const String URL_WEB = 'https://med-map.org';
   static const String URL_WEB_DETAIL_CASE = URL_WEB + '/cases-analysis/detail';
   static const String URL_WEB_DETAIL_PRODUCT = URL_WEB + '/product-detail';
-  static const String BASE_URL = 'http://192.168.50.100:3333';
-  // static const String BASE_URL = 'https://api.med-map.org';
+  // static const String BASE_URL = 'http://192.168.50.100:3333';
+  static const String BASE_URL = 'https://api.med-map.org';
   // static const String BASE_URL = 'https://api-medmap.mandatech.co.id';
   // static const String BASE_URL = 'https://be-mdmap.mandatech.co.id/';
   static const String URL_API = BASE_URL + '/v1';
